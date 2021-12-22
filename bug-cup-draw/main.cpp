9efbff02f0382d58451f97fddd116a114e5f058d
Eduardo Castro <eduardo@nucleus.eti.br>
2021-12-22 20:56:41 -0300

    Initial Commit

---
+++ bug-cup-draw/main.cpp
@@ -0,0 +1,14 @@
+//
+//  main.cpp
+//  bug-cup-draw
+//
+//  Created by Eduardo Castro on 22/12/21.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
